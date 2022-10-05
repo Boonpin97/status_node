060f1504cabe0447f627d60c2a241c7f20077f79
Boonpin97 <pohboonpin123@gmail.com>
2022-10-05 19:27:00 +0800

    added publisher

--- src/chatter_status.py
+++ src/chatter_status.py
@@ -1,14 +1,28 @@
+#!/usr/bin/env python
 import rospy
-from azimorph_msg.msg import Node
+from azimorph_msg.msg import Status, Topic
 
 def talker():
+    T = Topic()
+    T.name = "chatter1"
+    T.freq = 10
     msg = Status()
-    msg.Topics[0].name = "chatter1"
+    msg.Topics.append(T)
+    # msg.Topics[0].name = "chatter1"
+    # msg.Topics[0].freq = 10
+    # msg.Topics[0].status = 1
+    # msg.Topics[1].name = "chatter2"
+    # msg.Topics[1].freq = 20
+    # msg.Topics[1].status = 1
+    # msg.Nodes[0].name = "talker1"
+    # msg.Nodes[0].status = 1
+    # msg.Nodes[1].name = "talker2"
+    # msg.Nodes[1].status = 1
+
     pub = rospy.Publisher('chatter1', Status, queue_size=10)
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(10) # 10hzs
     while not rospy.is_shutdown():
-        rospy.loginfo(msg)
         pub.publish(msg)
         rate.sleep()
 
--- src/status_node.py
+++ src/status_node.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
 from distutils.log import info
-import rospy, rostopic, rosnode
+from platform import node
+import rospy, rostopic, rosnode, rospkg
 import yaml
 import sys
 import threading
 from std_msgs.msg import String
+from azimorph_msg.msg import Status, Topic, Node
+
 
 message_type = {
 	"String": String
@@ -14,20 +17,48 @@ message_type = {
 class simple_class:
 
 	def __init__(self):
-		yaml_file = open("catkin_ws/src/status_node/src/params.yaml","r")
+		r = rospkg.RosPack()
+		yamlFileDir = r.get_path('status_node') + '/src/params.yaml'
+		yaml_file = open(yamlFileDir,"r")
 		self.yaml_dict = yaml.safe_load(yaml_file)
 		self.timeout = self.yaml_dict["Params"]["timeout"]
 		self.window_size = self.yaml_dict["Params"]["window_size"]
 		self.topics_dict = self.yaml_dict["Topics"]
 		self.nodes_dict = self.yaml_dict["Nodes"]
 		self.r = rostopic.ROSTopicHz(self.window_size)
-		print(self.nodes_dict)
+		self.pub = rospy.Publisher('status', Status, queue_size=10)
 		for topic, info in self.topics_dict.items():
 			rospy.Subscriber(topic, message_type[info["type"]], self.callBack, callback_args = topic)
 			info["timer"] = threading.Timer(self.timeout, self.timeoutFunc, [topic])
 			info["status"] = 0 
 			info["freq"] = 0
 			info["timer"].start()
+
+		while not rospy.is_shutdown():
+			self.main_loop()
+			rospy.Rate(10).sleep()	
+		
+
+	def main_loop(self):
+		self.checkAllTopicStatus()
+		self.checkAllNodeStatus()
+		self.publishTopicsAndNodes()
+
+
+	def publishTopicsAndNodes(self):
+		msg = Status()
+		for topic, info in self.topics_dict.items():
+			t = Topic()
+			t.name = topic
+			t.status = info["status"]
+			t.freq = info["freq"]
+			msg.Topics.append(t)
+		for node, info in self.nodes_dict.items():
+			n = Node()
+			n.name = node
+			n.status = info["status"]
+			msg.Nodes.append(n)
+		self.pub.publish(msg)
 
 	def callBack(self, data, topic):
 		self.r.callback_hz(data, topic)
@@ -57,8 +88,6 @@ class simple_class:
 			if self.topics_dict[topic]["status"] != 1:
 				rospy.logwarn("Topic: " + topic + " 's frequency is back to normal!")
 			self.topics_dict[topic]["status"] = 1
-		self.checkAllTopicStatus()
-		self.checkAllNodeStatus()
 
 	def timeoutFunc(self, topic):
 		rospy.logerr("No message received from " + str(topic) + " for the past " + str(self.timeout) + " second(s)")
@@ -72,23 +101,29 @@ class simple_class:
 			elif info["status"] == 1:
 				status = "Alive"
 			elif info["status"] == 2:
-				status = "Slow Frequency"
+				status = "Slow Freq"
 			elif info["status"] == 3:
-				status = "Fast Frequency"
+				status = "Fast Freq"
 			else:
 				status = "Unknown"
 			freq = info["freq"]
-			message += str("\nStatus:{1:20} Last Frequency:{2:3}   Topic:{0:30} ".format(topic, status, freq))
+			message += str("\nTopic:{0:30} Status:{1:10}  Frequency:{2:3} ".format(topic, status, freq))
 		rospy.loginfo(message)
 
 	def checkAllNodeStatus(self):
+		message = "\nCurrent information of all nodes of interest:"
 		current_node_ls = rosnode.get_node_names()
 		for node, info in self.nodes_dict.items():
 			if node in current_node_ls:
 				info["status"] = 1
+				status = "Alive"
 			else:
 				info["status"] = 0
-		print("Nodes: ",self.nodes_dict)
+				status = "Dead"
+			message += str("\nNode:{0:31} Status:{1:1} ".format(node, status))
+		rospy.loginfo(message)
+
+
 
 def main(args):
 	rospy.init_node('simple_class', anonymous=False)
@@ -96,7 +131,7 @@ def main(args):
 	try:
 		rospy.spin()
 	except KeyboardInterrupt:
-		rospy.logwarn("Shutting down")
+		rospy.logwarn("Shutting down 2")
 
 if __name__ == '__main__':
     main(sys.argv)
